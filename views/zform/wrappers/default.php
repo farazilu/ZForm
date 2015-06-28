c53a09bd5da4d4c84ba76b3743b4d73f85668956
faraz <farazilu@gmail.com>
2015-06-28 11:39:08 +0100

    adjust label space

--- views/zform/wrappers/default.php
+++ views/zform/wrappers/default.php
@@ -1,8 +1,8 @@
 <?php defined('SYSPATH') or die('No direct script access.'); ?>
 
 <div class="form-group <?php echo $error ? 'error ' : ''; ?>field-<?php echo $field->id; ?>" id="field_<?php echo $field->id; ?>">
-    <?php echo $field->form_label(array('class' => 'col-sm-2 control-label')); ?>
-    <div class="col-sm-10">
+    <?php echo $field->form_label(array('class' => 'col-sm-3 control-label')); ?>
+    <div class="col-sm-9">
 		<?php echo $field->form_field(); ?>
 		<?php echo sprintf('<p class="help-block">%s</p>', $error ? $error : $help_text); ?>
     </div>
